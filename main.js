f52a0f6464f41f0873883de41871cb44e7d93fb0
tigg797 <98042739+tigg797@users.noreply.github.com>
2024-06-15 12:21:57 +0100

    Add files via upload
    final project

--- main.js
+++ main.js
@@ -10,15 +10,51 @@ Webcam.set({
 })
 
 camera = document.getElementById("camera");
-
 Webcam.attach('#camera')
+
+function modelLoaded() {
+    console.log("The model is loaded");
+}
 
 function take_snapshot() {
     Webcam.snap(function(data_uri) {
         document.getElementById("person").innerHTML = "<img id='captured_image' src='"+data_uri+"'/>"
     });
+    document.getElementById("identify_button").style.opacity=1;
 }
 
 console.log("the ml5 version qu'on est using is:"+ml5.version);
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/3DrZchUpI/model.json", modelLoaded);
 //linking ml5 and teachable machine je crois
+
+//Part 3:
+
+function identify() {
+    image = document.getElementById("captured_image");
+    classifier.classify(image,gotResult);
+    //classify(document.gEBI(...))
+}
+
+function gotResult(error, result) {
+    if(error){
+        console.error(error); //first parameter
+    } else {
+        console.log(result); //second parameter
+        var cestquoi = result[0].label
+        var celuici = " "
+        if(cestquoi == "A") {
+            celuici = "Mumma"
+        } else if(cestquoi == "B") {
+            celuici = "Papa"
+        } else if(cestquoi == "C") {
+            celuici = "Raj"
+        } else if(cestquoi == "D") {
+            celuici = "Dadi"
+        }
+        document.getElementById("personspan").innerHTML=celuici;
+        var percentAcc = result[0].confidence.toFixed(3);
+        percentAcc = percentAcc*100+"%";
+        document.getElementById("confidentspan").innerHTML=percentAcc;
+    }
+};
+
